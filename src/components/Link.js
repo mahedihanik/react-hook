3ef852eb376be22f8bb0ba7b204f8d1ed111ba86
Mahedi H Anik <mahedi@asl.aero>
2022-02-20 17:53:53 +0600

    routing setup eith core link

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 
 const Link = ({className, href, children}) => {
-    return <a className={className} href={href}>{children}</a>
+
+    const onClick = (event) => {
+        if (event.metaKey || event.ctrlKey){
+            return;
+        }
+        event.preventDefault();
+        window.history.pushState({},'',href);
+        const navEvent = new PopStateEvent('popstate');
+        window.dispatchEvent(navEvent);
+
+    }
+
+    return <a onClick={onClick} className={className} href={href}>{children}</a>
 };
 
 export default Link;
